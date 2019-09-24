750d0080ef874e3802590022d87fa7c2a91cb8ca
Rafa <reshresh@gmail.com>
2019-09-24 13:30:51 +0300

    create overlay not working

---
+++ src/overlay.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center">
+    <v-overlay :value="loading">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
+  </div>
+</template>
+<script>
+  Vue.console.log('in overlay')
+  export default {
+    data: () => ({
+      loading: false,
+    }),
+  }
+</script>
